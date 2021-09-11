74188cc68ae6fc73fb41fa74e31c5461cf1c85fe
zijun.y <zijun.y@antgroup.com>
2021-09-11 18:31:15 +0800

    feat: loading

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,5 +1,5 @@
 import { View, Button } from '@tarojs/components'
-import { useState } from 'react'
+import { useCallback, useState } from 'react'
 import {foodList} from '../../constants/food'
 import './index.less'
 
@@ -11,16 +11,35 @@ const getRandom = () => {
 
 const FC = () => {
   const [food, setFood] = useState(getRandom())
+  const [loading, setLoading] = useState(false)
 
   const reRandom = () => {
     const newRandom = getRandom()
     setFood(newRandom)
   }
 
+  let clock
+  const every3Sec = () => {
+    clock = setInterval(() => {
+      console.log('~~~~~~~~~~~~~~~~~~~~~~ interval');
+      reRandom()
+    }, 100)
+  }
+
+  const handleClick = useCallback(() => {
+    setLoading(true)
+    every3Sec()
+    setTimeout(() => {
+      console.log('~~~~~~~~~~~~~~~~~~~~~~ stop');
+      clearInterval(clock)
+      setLoading(false)
+    }, 2000)
+  }, [])
+
   return <View className='container'>
-    <View className='content'>{food}</View>
+    <View className={`content ${loading ? 'loading' : null}`}>{food}</View>
     <View className='footer'>
-      <Button type='primary' onClick={reRandom}>再摇一次</Button>
+      <Button type='primary' onClick={handleClick}>再摇一次</Button>
     </View>
   </View>
 }
